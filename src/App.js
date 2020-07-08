d223f33f53354641926a78f32273a5a2c3f4d8d7
Sriya <tsriya6@gmail.com>
2020-07-08 11:44:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import  './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    dice:1
+  }
+  throwDice = () =>{
+    const diceValue = Math.floor(Math.random()*6 + 1);
+    this.setState({
+      dice: diceValue
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <div className='container'>
+              <div className='diceContainer'>
+                <img className='dice' alt='dice-1' src={require(`./assets/dice-${this.state.dice}.png`)} />
+              </div>
+              <div className='button-container'>
+                <button onClick= {this.throwDice} className='DiceBtn'>Let's Play</button>
+              </div>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/component/dice.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Dice extends Component{
+    render(){
+        return(
+            <div className='dice'></div>
+        )
+    }
+}
+export default Dice;
